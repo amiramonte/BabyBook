38f6580854120f5535bbd583fa7c29999e22edaf
ZETA <ben.r.dominguez@gmail.com>
2022-02-10 15:08:14 -0800

    added script to add baby name to site

--- public/js/dash.js
+++ public/js/dash.js
@@ -1,7 +1,7 @@
 const apt = []
 const fam = []
 
-let famInput = document.getElementById('bbname').value
+let family = document.getElementById('exist-baby')
 
 console.log('dash script')
 console.log('===========')
@@ -10,6 +10,17 @@ $('#regApt').on('click', function() {
     console.log('made a new appointment!')
 })
 
-$('#regBaby').on('click', function(event) {
+$('#regBaby').on('click', function() {
+    let famInput = document.getElementById('baby-name').value
     console.log('I had another kid!')
+
+    fam.push(famInput)
+    let lastChild = fam.slice(-1)
+
+    lastChild.forEach((child) => {
+        let list = document.createElement('li')
+        list.innerText = child
+        family.append(list)
+    })
+    console.log(fam)
 })
