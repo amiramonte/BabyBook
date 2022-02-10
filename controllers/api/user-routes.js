5f899bffd5348c64e89c6eec2003f3380fac36cc
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-02-10 13:32:56 -0800

    added get routes for user, thread, and comment in respective files

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
-const {User} = require('../../models');
+const {User, Thread, Comment} = require('../../models');
 
 
+// The `/api/user` endpoint
 
+// GET route for all users
+router.get('/', async(req,res) => {
+    try {
+        const userData = await User.findAll({
+            include: [Thread, Comment]
+        })
+        return res.status(200).json(userData);
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+});
 
 
 
