acbf523530306de709b4138a5e7426165576fb0d
ZETA <ben.r.dominguez@gmail.com>
2022-02-10 14:27:32 -0800

    added the most basic stuff, literally console logging and fixed css linking

---
+++ public/js/dash.js
@@ -0,0 +1,15 @@
+const apt = []
+const fam = []
+
+let famInput = document.getElementById('bbname').value
+
+console.log('dash script')
+console.log('===========')
+
+$('#regApt').on('click', function() {
+    console.log('made a new appointment!')
+})
+
+$('#regBaby').on('click', function(event) {
+    console.log('I had another kid!')
+})
