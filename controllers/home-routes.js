c6dddecf73b78909fd31b2094f4b929d0ca1bc80
ZETA <ben.r.dominguez@gmail.com>
2022-02-11 15:02:22 -0800

    added functionality to the register baby btn on dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,18 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-    res.render('./dash')
+    res.render('dash')
 })
 
 router.get('/sign-in', async (req, res) => {
-    res.render('./signin')
+    res.render('signin')
 })
 
 router.get('/sign-up', async (req, res) => {
-    res.render('./signup')
+    res.render('signup')
 })
 router.get('/forum', async (req, res) => {
-    res.render('./forum')
+    res.render('forum')
 })
 
 module.exports = router
