e0090debd21c2b36b1f6b1534a3480e916f466f5
Lorena <lzuniga1315@gmail.com>
2022-02-12 13:25:40 -0800

    wip

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const router = require('express').Router();
-const {User, Thread, Comment} = require('../../models');
+const { User, Thread, Comment } = require('../../models');
 
 
 // The `/api/user` endpoint
 
 // GET route for all users
-router.get('/', async(req,res) => {
+router.get('/', async (req, res) => {
     try {
         const userData = await User.findAll({
             include: [Thread, Comment]
         })
-         res.status(200).json(userData);
+        res.status(200).json(userData);
     } catch (error) {
-         res.status(400).json(error);
+        res.status(400).json(error);
     }
 });
 
 
 
 // POST route for create new user
-router.post('/sign-up', async(req, res) => {
+router.post('/sign-up', async (req, res) => {
     try {
         const newUser = await User.create({
             firstname: req.body.firstname,
@@ -31,8 +31,8 @@ router.post('/sign-up', async(req, res) => {
             password: req.body.password
         })
 
-         res.status(200).json(newUser);
-    
+        res.status(200).json(newUser);
+
     } catch (error) {
         res.status(400).json(error);
     }
@@ -42,22 +42,22 @@ router.post('/sign-up', async(req, res) => {
 
 
 // SIGN IN route for user
-router.post('/sign-in', async(req, res) => {
+router.post('/sign-in', async (req, res) => {
     try {
         const currentUser = await User.findOne({
             where: {
-               email: req.body.email, 
+                email: req.body.email,
             },
         })
 
         if (!currentUser) {
-             res.status(400).json({message: "Invalid Email/Password. Please check and try again."})
+            res.status(400).json({ message: "Invalid Email/Password. Please check and try again." })
         }
 
         const validPassword = await bcrypt.compare(req.body.password, currentUser.password);
 
         if (!validPassword) {
-             res.status(400).json({message: "Invalid Email/Password. Please check and try again."})
+            res.status(400).json({ message: "Invalid Email/Password. Please check and try again." })
         }
 
         req.session.user = {
@@ -66,48 +66,80 @@ router.post('/sign-in', async(req, res) => {
             email: currentUser.email
         }
 
-         res.status(200).json(currentUser);
+        res.status(200).json(currentUser);
 
     } catch (error) {
-         res.status(400).json(error)
+        res.status(400).json(error)
     }
 })
 
 
 // SIGN OUT route for user
-router.get('/sign-out', (req,res) => {
+router.get('/sign-out', (req, res) => {
     try {
         req.session.destroy();
-        res.json({msg: 'You have successfully signed out!'})    
+        res.json({ msg: 'You have successfully signed out!' })
     } catch (error) {
         res.status(400).json(error);
     }
 })
 
 
-
-
-
-router.get('/showsessions', (req,res) => {
+// show sessions
+router.get('/showsessions', (req, res) => {
     res.json(req.session);
 })
 
 
-
-
-// GET route for single user
-router.get('/:id', async (req, res) => {
-    try {
-      const singleUser = await User.findByPk(req.params.id, {
-        include: [Thread, Comment]
-      })
-       res.status(200).json(singleUser)
-    } catch (error) {
-         res.status(400).json(error)
+// GET route for user who forgot password
+router.get('/forgot-password', function (req, res, next) {
+    res.render('user/forgot-password', {});
+});
+// post route 
+router.post('/forgot-password', async function (req, res, next) {
+    var email = await User.findOne({ where: { email: req.body.email } });
+    if (email == null) {
+        return res.json({ status: 'check email' });
     }
-  });
+    await ResetToken.update({
+        used: 1
+    },
+        {
+            where: {
+                email: req.body.email
+            }
+        })
+    
+
+    //create email
+  const message = {
+    from: process.env.SENDER_ADDRESS,
+    to: req.body.email,
+    replyTo: process.env.REPLYTO_ADDRESS,
+    subject: process.env.FORGOT_PASS_SUBJECT_LINE,
+    text: 'To reset your password, please click the link below.\n\nhttps://'+process.env.DOMAIN+'/user/reset-password?token='+encodeURIComponent(token)+'&email='+req.body.email
+};
+
+//send email
+transport.sendMail(message, function (err, info) {
+   if(err) { console.log(err)}
+   else { console.log(info); }
+});
+});
+
+    // GET route for single user
+    router.get('/:id', async (req, res) => {
+        try {
+            const singleUser = await User.findByPk(req.params.id, {
+                include: [Thread, Comment]
+            })
+            res.status(200).json(singleUser)
+        } catch (error) {
+            res.status(400).json(error)
+        }
+    });
 
 
 
 
-module.exports = router;
+    module.exports = router;
