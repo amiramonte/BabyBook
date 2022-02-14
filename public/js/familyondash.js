1d46e07b8b813412afe6df0f6a610cf758ad726b
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-02-14 12:46:00 -0800

    added front end javascript for removal button functionality

--- public/js/familyondash.js
+++ public/js/familyondash.js
@@ -5,15 +5,20 @@ const removeFamilyMemberBtn = document.querySelector('#removeFamilyBtn');
 const removeFamilyMember = async function (event) {
     event.preventDefault();
 
-    const response = await fetch('/api/user/removeuser/:id', {
+    const response = await fetch('/api/user/removeuser', {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
       });
 
+      console.log(response);
+
       if (response.ok) {
-          
+          document.location.replace('/dashboard')
       } else {
-          
+          alert('We were not able to remove your family member')
       }
 
   }
+
+
+  removeFamilyMemberBtn.addEventListener('click', removeFamilyMember);
